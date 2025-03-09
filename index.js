c3ce75a2743721cd545aae91b2849ff1a94a92b6
Hugh Palmer <hughpalmerproduction@gmail.com>
2025-03-10 08:55:51 +1100

    test for lambda ses

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+function initializeNavigation() {
   const nav = document.querySelector("#mainNavbar");
   const hamburger = document.querySelector(".hamburger");
   const navList = document.querySelector("nav ul");
@@ -15,4 +15,46 @@ document.addEventListener("DOMContentLoaded", () => {
     navList.classList.toggle("active");
     hamburger.classList.toggle("active");
   });
+}
+
+// Send contact form to Lambda for Ses
+async function initializeContactForm() {
+  const form = document.getElementById("contact-form");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const formData = {
+      name: document.getElementById("form-name").value,
+      email: document.getElementById("form-email").value,
+      message: document.getElementById("form-message").value,
+    };
+    const jsonData = JSON.stringify(formData);
+    try {
+      const response = await fetch(
+        "https://q9ut7p24g0.execute-api.ap-southeast-2.amazonaws.com/dev/contact",
+        {
+          method: "POST",
+          headers: { "content-type": "application/json" },
+          body: jsonData,
+        }
+      );
+      const res = await response.json();
+      if (res.success) {
+        alert("EMAIL SENT SUCCESSFUL");
+      } else {
+        alert("EMAIL SEND UNSUCCESSFUL");
+      }
+    } catch (e) {
+      console.error(`Error sending form ${e}`);
+    }
+  }
+
+  form.addEventListener("submit", handleSubmit);
+}
+
+// Initialize everything when DOM is ready
+document.addEventListener("DOMContentLoaded", () => {
+  initializeNavigation();
+  initializeContactForm();
 });
